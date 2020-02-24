f1a045dcdc23289f774f54468c70730fcc00e440
jonheslop <jon.heslop@pusher.com>
2020-02-24 17:57:17 +0000

    Label component

---
+++ test/components/input_label_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class InputLabelComponentTest < ActionView::Component::TestCase
+  test "component renders something useful" do
+    # assert_equal(
+    #   %(<span title="my title">Hello, components!</span>),
+    #   render_inline(InputLabelComponent, attr: "value") { "Hello, components!" }.css("span").to_html
+    # )
+  end
+end
