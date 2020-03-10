9607db8c4eac0c6cdb1a0b41674f11b00ff5f87c
jonheslop <jon.heslop@pusher.com>
2020-03-10 16:00:02 +0000

    Make component classList building more like others and add test

--- app/components/box_component.rb
+++ app/components/box_component.rb
@@ -3,8 +3,11 @@
 class BoxComponent < ActionView::Component::Base
   validates :content, presence: true
 
-  def initialize(classes: '')
-    @classes = classes
+  def initialize(classes: nil)
+    base_classes = %w[br2 ba b--smoke-light pa5]
+    all_classes = base_classes
+    all_classes << classes unless classes.nil?
+    @classes = all_classes.join(' ')
   end
 
   attr_reader :classes
--- test/components/box_component_test.rb
+++ test/components/box_component_test.rb
@@ -1,10 +1,27 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class BoxComponentTest < ActionView::Component::TestCase
-  test "component renders something useful" do
-    # assert_equal(
-    #   %(<span title="my title">Hello, components!</span>),
-    #   render_inline(BoxComponent, attr: "value") { "Hello, components!" }.css("span").to_html
-    # )
+  basic_box = %(<div class="br2 ba b--smoke-light pa5">
+  Hello, components!
+</div>)
+
+  test 'Box should render with base classes and contain passed content' do
+    assert_equal(
+      basic_box,
+      render_inline(BoxComponent.new) { 'Hello, components!' }.css('div').to_html
+    )
+  end
+
+  advanced_box = %(<div class="br2 ba b--smoke-light pa5 testing classes">
+  Hello, components!
+</div>)
+
+  test 'Box should render with base classes, passed classes and contain passed content' do
+    assert_equal(
+      advanced_box,
+      render_inline(BoxComponent.new(classes: 'testing classes')) { 'Hello, components!' }.css('div').to_html
+    )
   end
 end
