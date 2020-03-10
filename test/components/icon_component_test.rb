50a5b4c59e80ce32a7c6aef093444122a64fdbc6
jonheslop <jon.heslop@pusher.com>
2020-03-10 17:26:07 +0000

    Tests for icons

--- test/components/icon_component_test.rb
+++ test/components/icon_component_test.rb
@@ -1,10 +1,49 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class IconComponentTest < ActionView::Component::TestCase
-  test "component renders something useful" do
-    # assert_equal(
-    #   %(<span title="my title">Hello, components!</span>),
-    #   render_inline(IconComponent, attr: "value") { "Hello, components!" }.css("span").to_html
-    # )
+  test 'Icon component should have title' do
+    icon = render_inline(IconComponent.new(name: 'caret-down', title: 'Icon'))
+    assert_equal(
+      'Icon',
+      icon.css('title').text
+    )
+  end
+
+  test 'Icon title id and aria-labelledby should be equal' do
+    icon = render_inline(IconComponent.new(name: 'caret-down', title: 'Icon'))
+    assert_equal(
+      icon.css('title').attr('id').text,
+      icon.css('svg').attr('aria-labelledby').text
+    )
+  end
+
+  test 'Icon should have role="img"' do
+    icon = render_inline(IconComponent.new(name: 'caret-down', title: 'Icon'))
+    assert_equal(
+      icon.css('svg').attr('role').text,
+      'img'
+    )
+  end
+
+  test 'Icon should used passed size' do
+    icon = render_inline(IconComponent.new(name: 'caret-down', title: 'Icon', size: 64))
+    assert_equal(
+      icon.css('svg').attr('width').text,
+      '64'
+    )
+    assert_equal(
+      icon.css('svg').attr('height').text,
+      '64'
+    )
+  end
+
+  test 'Icon should used passed class' do
+    icon = render_inline(IconComponent.new(name: 'caret-down', title: 'Icon', classes: 'extra'))
+    assert_equal(
+      icon.css('svg').attr('class').text,
+      'extra'
+    )
   end
 end
