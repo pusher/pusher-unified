bf5b1656adcdb6f81fbec2bd74d5efeb342a4db7
jonheslop <jon.heslop@pusher.com>
2020-03-10 15:57:59 +0000

    Add test for divider pill component

--- test/components/divider_pill_component_test.rb
+++ test/components/divider_pill_component_test.rb
@@ -1,10 +1,22 @@
-require "test_helper"
+# frozen_string_literal: true
 
+require 'test_helper'
 class DividerPillComponentTest < ActionView::Component::TestCase
-  test "component renders something useful" do
-    # assert_equal(
-    #   %(<span title="my title">Hello, components!</span>),
-    #   render_inline(DividerPillComponent, attr: "value") { "Hello, components!" }.css("span").to_html
-    # )
+  basic_divider = %(<div class="divider nl5 nr5 mv5 tc">
+  <span class="bg-snow-dark br4 pv1 ph3 lh-solid f8 ba bw1 b--snow-dark">or</span>
+</div>)
+
+  test 'Divider should render with or by default' do
+    assert_equal(
+      basic_divider,
+      render_inline(DividerPillComponent.new).css('div').to_html
+    )
+  end
+
+  test 'Divider should render with passed text' do
+    assert_equal(
+      'or use email',
+      render_inline(DividerPillComponent.new(text: 'or use email')).text.strip
+    )
   end
 end
