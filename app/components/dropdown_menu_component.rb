ed468968542680c03037360e84f9997b54e747fb
jonheslop <jon.heslop@pusher.com>
2020-03-16 17:13:54 +0000

    Add attribute so can set dropdown menu position with class

--- app/components/dropdown_menu_component.rb
+++ app/components/dropdown_menu_component.rb
@@ -2,13 +2,14 @@
 
 class DropdownMenuComponent < ActionView::Component::Base
   include ApplicationHelper
-  def initialize(label:, wrapper_element: 'div', wrapper_classes: '', size: 'small', show_caret: true, children: {})
+  def initialize(label:, wrapper_element: 'div', wrapper_classes: '', size: 'small', show_caret: true, position_class: '', children: {})
     @label = label
     @wrapper_element = wrapper_element
     @wrapper_classes = wrapper_classes
     @size = size
     @id = "dropdown-#{SecureRandom.hex(4)}"
     @show_caret = show_caret
+    @position_class = position_class
     @children = children
   end
 end
