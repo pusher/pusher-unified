a46cd54e096a7736318ed54d9b6d338f4435aad9
jonheslop <jon.heslop@pusher.com>
2020-03-16 17:09:04 +0000

    Hide caret in breadcrumbs dropdown

--- app/components/dropdown_menu_component.rb
+++ app/components/dropdown_menu_component.rb
@@ -2,12 +2,13 @@
 
 class DropdownMenuComponent < ActionView::Component::Base
   include ApplicationHelper
-  def initialize(label:, wrapper_element: 'div', wrapper_classes: '', size: 'small', children: {})
+  def initialize(label:, wrapper_element: 'div', wrapper_classes: '', size: 'small', show_caret: true, children: {})
     @label = label
     @wrapper_element = wrapper_element
     @wrapper_classes = wrapper_classes
     @size = size
     @id = "dropdown-#{SecureRandom.hex(4)}"
+    @show_caret = show_caret
     @children = children
   end
 end
