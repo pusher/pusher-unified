5ad418f8e9c9d4011f477c31ef8c6605fb8e4d4d
jonheslop <jon.heslop@pusher.com>
2020-03-16 15:01:05 +0000

    Make the switch work for more than 2 options

--- app/components/switch_component.rb
+++ app/components/switch_component.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
 class SwitchComponent < ActionView::Component::Base
-  def initialize(name:,
-                 left_label:,
-                 right_label:,
-                 checked: 'left')
-
+  def initialize(name:, switches: [])
     @name = name
-    @left_id = "#{name}-#{SecureRandom.hex(4)}"
-    @right_id = "#{name}-#{SecureRandom.hex(4)}"
-    @left_label = left_label
-    @right_label = right_label
-    @checked = checked
+    @switches = switches
   end
 end
