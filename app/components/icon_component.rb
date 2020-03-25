87cdba5285892fa005872d545fc3c58230e4d945
jonheslop <jon.heslop@pusher.com>
2020-03-25 17:06:24 +0000

    Not all icons are square

--- app/components/icon_component.rb
+++ app/components/icon_component.rb
@@ -2,10 +2,12 @@
 
 class IconComponent < ActionView::Component::Base
   include ApplicationHelper
-  def initialize(name:, title:, size: 16, classes: '')
+  def initialize(name:, title:, size: 16, classes: '', width: nil, height: 'auto')
     @name = name
     @title = title
     @size = size
     @classes = classes
+    @width = width.nil? ? size : width
+    @height = height.nil? ? size : height
   end
 end
