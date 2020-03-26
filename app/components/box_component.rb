1f64c1ca2019094d216be95c4ddadd403c88a3f6
jonheslop <jon.heslop@pusher.com>
2020-03-26 16:52:54 +0000

    Padding size options for boxes

--- app/components/box_component.rb
+++ app/components/box_component.rb
@@ -3,11 +3,13 @@
 class BoxComponent < ActionView::Component::Base
   validates :content, presence: true
 
-  def initialize(classes: nil, dashed: false)
-    base_classes = %w[pa5 ba b--smoke-light]
+  def initialize(classes: nil, dashed: false, padding: 'large')
+    base_classes = %w[ba]
     all_classes = base_classes
-    all_classes << 'b--dashed bw1 br3' if dashed
-    all_classes << 'br2' unless dashed
+    all_classes << 'pa5' if padding == 'large'
+    all_classes << 'pv2 ph3' if padding == 'small'
+    all_classes << 'b--smoke b--dashed bw1 br4' if dashed
+    all_classes << 'b--smoke-light br2' unless dashed
     all_classes << classes unless classes.nil?
     @classes = all_classes.join(' ')
   end
